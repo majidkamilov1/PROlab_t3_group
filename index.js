8b6b66a5c190b640a2e8e379c0e2667738f604a3
AbuKarabaev <karabaevabdulloh1808@icloud.com>
2025-04-10 16:57:20 +0600

    first commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const the_right_combination = [
 
 const func = () => {
     const box = document.getElementById("box");
-    const ma=document.querySelector("#win-model")
+    const ma = document.querySelector("#win-model")
     const md = document.getElementById("w1")
     box.addEventListener("click", (e) => {
         if (e.target.classList.contains("box-0")) {
@@ -29,21 +29,21 @@ const func = () => {
             }
             setTimeout(() => {
                 const ooo = the_right_combination.some((item) => o.includes(item[0]) && o.includes(item[1]) && o.includes(item[2]));
-                if (ooo){
-                    md.textContent +=" o"
+                if (ooo) {
+                    md.textContent += " o"
                     ma.classList.toggle("hidden")
-                }; 
+                };
                 const xxx = the_right_combination.some((item) => x.includes(item[0]) && x.includes(item[1]) && x.includes(item[2]));
-                if (xxx){
-                    md.textContent +=" x"
+                if (xxx) {
+                    md.textContent += " x"
                     ma.classList.toggle("hidden")
                 }
 
-                if(o.length + x.length == 9 && md.textContent.at(-1)=="л"){
+                if (o.length + x.length == 9 && md.textContent.at(-1) == "л") {
                     md.textContent = "НИЧЬЯ"
                     ma.classList.toggle("hidden")
                     console.log(56789);
-                    
+
                 }
 
             }, 50)
@@ -59,19 +59,20 @@ let restart = () => {
     let box = document.querySelectorAll(".box-0")
     const md = document.getElementById("w1")
     let btn = document.querySelector(".btnh")
-    const ma=document.querySelector("#win-model")
-    btn.addEventListener("click",(e)=>{
-        box.forEach((x)=>{
-            if(x.children.item(1).classList.contains("flex")){
-                x.children.item(1).classList.replace("flex","hidden")
+    const ma = document.querySelector("#win-model")
+    btn.addEventListener("click", (e) => {
+        box.forEach((x) => {
+            if (x.children.item(1).classList.contains("flex")) {
+                x.children.item(1).classList.replace("flex", "hidden")
             }
-            else if(x.children.item(0).classList.contains("flex")){
-                x.children.item(0).classList.replace("flex","hidden")
+            else if (x.children.item(0).classList.contains("flex")) {
+                x.children.item(0).classList.replace("flex", "hidden")
             }
         })
         md.textContent = "выиграл"
-        o=[]
-        x=[]
+        o = []
+        x = []
+        i = 0
         ma.classList.toggle("hidden")
     })
 }
